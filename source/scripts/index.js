506657665fc703f081e029a6101088f42fd5b6e6
Юлия Агафонова <yuliya-agafonova-1997@mail.ru>
2024-02-24 19:31:30 +0300

    header

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,7 @@
-/* в этот файл добавляет скрипты*/
+const burgerButton = document.querySelector('.js-toggle-button');
+const nav = document.querySelector('.header__list');
+
+burgerButton.onclick = function () {
+  burgerButton.classList.toggle('is-active');
+  nav.classList.toggle('js-header-list');
+}
