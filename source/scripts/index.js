62fb587d54871ee72611c3a4e78b8c1532305845
Юлия Агафонова <yuliya-agafonova-1997@mail.ru>
2024-02-27 21:57:19 +0300

    catalog

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,16 +1,57 @@
+//Бургер-меню
 const burgerButton = document.querySelector('.js-toggle-button');
 const nav = document.querySelector('.header__list');
 
-burgerButton.onclick = function () {
+function ButtonClick () {
   burgerButton.classList.toggle('is-active');
   nav.classList.toggle('js-header-list');
 };
 
+burgerButton.addEventListener('click', ButtonClick);
+
+//Visually-hidden
+const paginationButtonText1 = document.querySelector('.catalog__pagination-button-text--1');
+const paginationButtonText2 = document.querySelector('.catalog__pagination-button-text--2');
+const paginationButtonIcon1 = document.querySelector('.catalog__pagination-icon--1');
+const paginationButtonIcon2 = document.querySelector('.catalog__pagination-icon--2');
 const userLinkText1 = document.querySelector('.header__user-text--1');
 const userLinkText2 = document.querySelector('.header__user-text--2');
 
-if (window.matchMedia('(min-width: 1440px)').matches) {
-  userLinkText1.classList.remove('visually-hidden');
-  userLinkText2.classList.remove('visually-hidden');
-};
+window.addEventListener("resize", (e) => {
+  const windowWidth = window.innerWidth;
+  if (windowWidth > 768) {
+    paginationButtonText1.classList.remove('visually-hidden');
+    paginationButtonText2.classList.remove('visually-hidden');
+    paginationButtonIcon1.classList.add('visually-hidden');
+    paginationButtonIcon2.classList.add('visually-hidden');
+  }
+  if (windowWidth < 768) {
+    paginationButtonText1.classList.add('visually-hidden');
+    paginationButtonText2.classList.add('visually-hidden');
+    paginationButtonIcon1.classList.remove('visually-hidden');
+    paginationButtonIcon2.classList.remove('visually-hidden');
+   }
+});
+
+window.addEventListener("resize", (e) => {
+  const windowWidth = window.innerWidth;
+  if (windowWidth > 1440) {
+    userLinkText1.classList.remove('visually-hidden');
+    userLinkText2.classList.remove('visually-hidden');
+  }
+  if (windowWidth < 1440) {
+    userLinkText1.classList.add('visually-hidden');
+    userLinkText2.classList.add('visually-hidden');
+   }
+});
+
+//Disabled
+const filterControl = document.querySelectorAll('.filter__control');
+const filterRadio = document.querySelectorAll('.filter__radio');
+
+function disabledRadio () {
+  if (filterRadio.disabled = true) {
+    filterControl.classList.add('disabled');
+  }
+}
 
